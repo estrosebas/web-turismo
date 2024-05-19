abae0753a3a6eaa30ba582606ad31a576504050e
estrosebas <estrosebas@gmail.com>
2024-05-19 13:18:03 -0500

    colores y hover

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function seleccionar(link) {
     });
     link.classList.add("seleccionado");
 }
+
+document.getElementById('btn-sol').addEventListener('click', function() {
+    document.body.style.backgroundColor = "#333333";
+    document.querySelectorAll('.letreros').forEach(function(letrero) {
+        letrero.style.color = "#FFFFFF"; // Amarillo
+        
+    });
+});
+
+document.getElementById('btn-nube').addEventListener('click', function() {
+    document.body.style.backgroundColor = "#2C3E50"; // Azul
+    document.querySelectorAll('.letreros').forEach(function(letrero) {
+        letrero.style.color = '#FFFFFF'; // Azul
+        
+    });
+});
+
+document.getElementById('btn-luna').addEventListener('click', function() {
+    document.body.style.backgroundColor = "#9B59B6";
+    document.querySelectorAll('.letreros').forEach(function(letrero) {
+        letrero.style.color = "#333333"; // Morado
+    });
+});
